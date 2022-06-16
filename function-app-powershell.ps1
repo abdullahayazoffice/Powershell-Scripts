6adcde991a35d54be63dd82ab42e33a410e415be
unknown <LHRLT-8566$@systemsltd.local>
2022-06-16 20:27:05 +0500

    initital commit

--- function-app-powershell.ps1
+++
@@ -1,7 +0,0 @@
- $allWebApps =Get-AzFunctionApp
-  //$AllMyWebApps = @()
-   foreach ($app in $allWebApps){
-
-az functionapp vnet-integration list --name $app.Name --resource-group $app.ResourceGroupName | export-csv function-app-vnet.csv
-
-   }
--- testing1.ps1
+++
@@ -1,8 +0,0 @@
- $allWebApps =Get-AzFunctionApp
-
-   foreach ($app in $allWebApps){
-
-  $Properties = az functionapp vnet-integration list --name $app.Name --resource-group $app.ResourceGroupName | Add-content file1.csv
-  
-  
-   }
